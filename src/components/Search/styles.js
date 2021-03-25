746f9e65cc8b6e58d7bb2a4e146976f3258a6db6
brnaldomesi <brnaldomesi1@gmail.com>
2021-03-25 21:38:47 +0900

    Update feed page by adding Search component

---
+++ src/components/Search/styles.js
@@ -0,0 +1,41 @@
+import {Size} from 'src/styles';
+
+import {StyleSheet} from 'react-native';
+import { fontWeightBold } from '../../styles';
+
+const styles = StyleSheet.create({
+  feed: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  feedImgCorner: {
+    marginLeft: 10,
+  },
+  inputCorner: {
+    marginRight: 10,
+    marginLeft: 10,
+    flexGrow: 1,
+  },
+  input: {
+    alignSelf: 'center',
+  },
+  text: {
+    marginLeft: 10,
+    fontSize: 25,
+    fontWeight: '900',
+    color: '#05174f',
+  },
+  search: {
+    width: 30,
+    height: 30,
+  },
+  searchCorner: {
+    marginTop: 10,
+    marginRight: 10,
+    width: 30,
+    height: 30,
+  },
+  face: {},
+});
+
+export default styles;
